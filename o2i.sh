f9e81ba4f6936c0f1fdb0d8e24dbc64f59746375
pedro <phneveslima@hotmail.com>
2023-03-09 09:35:27 -0300

    Uncommenting stuff

--- o2i.sh
+++ o2i.sh
@@ -14,20 +14,20 @@ if [[ "$1" == "headers" ]]; then
     if [[ -f "$2" ]]; then
         while IFS= read -r linha; do
             echo "$linha"
-            # mkdir "$linha"
-            # cd "$linha"
+            mkdir "$linha"
+            cd "$linha"
             python3 simple-security-headers.py -u "$linha" >> "$linha.ansi"
             ansilove -o "$linha.png" "$linha.ansi"
-            # cd ..
+            cd ..
         done < "$2"
     else
         alvo="$2"
         echo "$alvo"
-        # mkdir "$alvo"
-        # cd "$alvo"
+        mkdir "$alvo"
+        cd "$alvo"
         python3 simple-security-headers.py -u "$alvo" >> "$alvo.ansi"
         ansilove -o "$alvo.png" "$alvo.ansi"
-        # cd ..
+        cd ..
     fi
 
 elif [[ "$1" == "sslscan" ]]; then
@@ -39,20 +39,20 @@ elif [[ "$1" == "sslscan" ]]; then
     if [[ -f "$2" ]]; then
         while IFS= read -r linha; do
             echo "$linha"
-            # mkdir "$linha"
-            # cd "$linha"
+            mkdir "$linha"
+            cd "$linha"
             sslscan $linha >> "$linha.ansi"
             ansilove -o "$linha.png" "$linha.ansi"
-            # cd ..
+            cd ..
         done < "$2"
     else
         alvo="$2"
         echo "$alvo"
-        # mkdir "$alvo"
-        # cd "$alvo"
+        mkdir "$alvo"
+        cd "$alvo"
         sslscan "$alvo" >> "$alvo.ansi"
         ansilove -o "$alvo.png" "$alvo.ansi"
-        # cd ..
+        cd ..
     fi
 
 else
